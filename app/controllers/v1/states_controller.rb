74ee6749919d3a4704016107146d20fecaff39e9
Abdellah Ramadan <ajrmdn@gmail.com>
2019-08-23 09:30:24 -0700

    add randomization to the app.

--- app/controllers/v1/states_controller.rb
+++ app/controllers/v1/states_controller.rb
@@ -16,8 +16,35 @@ class V1::StatessController < ApplicationController
     @state = State.create(state_params)
     json_response(@state)
     render status: 200, json: {
-     message: "State park has been created."
-     }
+      message: "State park has been created."
+    }
+  end
+
+  def update
+    @state = State.find(params[:id])
+    @state.update(state_params)
+    render status: 200, json: {
+      message: "State park has been updated successfully."
+    }
+  end
+
+  def destroy
+    @state = State.find(params[:id])
+    @state.destroy
+    render status: 200, json: {
+      message: "State park has been destroyed."
+    }
+  end
+
+  def random
+    @state = State.random
+    json_response(@state)
+  end
+
+  private
+
+  def state_params
+    params.permit(:name)
   end
   
 end
